68275607ccfe511884eb34e47148760c7a0e9353
Tina Huynh <tinamchuynh@gmail.com>
2024-10-06 17:12:03 -0700

    add database

---
+++ faker.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS faker_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE faker_db;
+CREATE TABLE IF NOT EXISTS person (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255),
+  prefix VARCHAR(10),
+  suffix VARCHAR(10),
+  address VARCHAR(255),
+  email VARCHAR(255),
+  phone_number VARCHAR(20),
+  ssn VARCHAR(20),
+  license_plate VARCHAR(20)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS job (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(255),
+  company_name VARCHAR(255),
+  address VARCHAR(255),
+  domain_name VARCHAR(255),
+  CONSTRAINT job_id FOREIGN KEY (id) REFERENCES person (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
