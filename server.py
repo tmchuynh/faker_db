7730fa070af815ac7a46a4d5d02ffdbccc11bca3
Tina Huynh <tinamchuynh@gmail.com>
2024-10-08 10:47:10 -0700

    "Updated Pipfile and Pipfile.lock, removed app.py, and modified server.py"

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from app import create_app
+
+# Create the Flask app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run( debug = True)
+    # Run the app in debug mode
+    app.run(debug=True)
